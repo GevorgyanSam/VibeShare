409501312484c10d24490027d050d2e582b19653
GevorgyanSam <SamvelGevorgyanPC@gmail.com>
2023-05-14 17:25:57 +0400

    VibeShare (Starting Home Page Interface) V(1.3.0)

--- build/home.php
+++ build/home.php
@@ -19,7 +19,34 @@
     <title>VibeShare | Home</title>
 </head>
 <body>
-    
+
+
+                            <!-- ----- ---------- ----- -->
+                            <!-- ----- Navigation ----- -->
+                            <!-- ----- ---------- ----- -->
+
+
+    <header>
+        <nav>
+            <div class="navParent">
+                <div class="logoParent">
+                    <div class="logo">
+                        <a href="./home.php">VibeShare</a>
+                    </div>
+                </div>
+                <div class="userParent">
+                    <a href="./home.php">
+                        <div>
+                            <h3>User</h3>
+                        </div>
+                        <div>
+                            <img src="./assets/defaultuser.jpg">
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </header>
     <script src="./lib/jquery-3.6.4.min.js"></script>
     <script src="./js/home.js"></script>
 </body>
